67797725e74a834454d53b7cf6d8adabe80faee8
Mohammed <mohammed43312@gmail.com>
2018-11-20 14:18:24 -0500

    Add .gitignore file.

--- src/Sort.java
+++ src/Sort.java
@@ -5,25 +5,30 @@ public class Sort
         for (int i = 0; i < arr.length - 1; i++)
         {
             int lowPosition = i;
-            for (int j = i + 1; i < arr.length; j++)
-            {
+            for (int j = i + 1; j < arr.length; j++)
                 if (arr[j] < arr[lowPosition])
+                {
                     lowPosition = j;
-            }
+                }
             swap(i, lowPosition, arr);
         }
+    }
 
-        public static void swap ( int indexA, int indexB, int[] arr)
+    public static void swap( int indexA, int indexB, int[] arr)
+    {
+        int temp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = temp;
+    }
+
+    public static String printArr ( int[] arr)
+    {
+        String output = "";
+        for (int num : arr)
         {
-            int temp = arr[indexA];
-            arr[indexA] = arr[indexB];
-            arr[indexB] = temp;
+            output = output + num + " ";
         }
-        public static void printArr ( int[] arr)
-        {
-            for (int num : arr) {
-                System.out.print(num + "");
-            }
-        }
+        return output;
     }
 }
+
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test
+{
+    public static void main(String[] args)
+    {
+        int[] arr1= {500,100,20,54,546,5456,85461,41,621,984,8974,89416};
+        Sort.ascendingSort(arr1);
+        System.out.print(Sort.printArr(arr1));
+    }
+}
